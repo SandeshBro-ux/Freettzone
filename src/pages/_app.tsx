fa12c2bbb695fc16e1dac143d8d10a89265b4f87
Your Name <you@example.com>
2025-05-25 23:36:54 +0545

    Add custom Document for favicon and remove redundant link in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Free Tiktok Zone</title>
         <meta name="description" content="Unlock the full potential of TikTok content. Paste any TikTok video URL to instantly fetch details, view statistics, and download videos in various formats." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/Freezone.png" />
       </Head>
       <Component {...pageProps} />
     </>
